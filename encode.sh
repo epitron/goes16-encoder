3df2f7ca6d4e86a6cee0e84b5d50a5961cb7a25d
epitron <chris@ill-logic.com>
2018-05-06 16:59:26 +0000

    'encode.sh' updated

--- encode.sh
+++ encode.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ffmpeg -framerate 1 -pattern_type glob -i '*.jpg' -c:v libx264 -r 24 -t 5 -pix_fmt yuv420p out.mp4
+ffmpeg -framerate 1 -pattern_type glob -i 'pics/*.jpg' -c:v libx264 -r 24 -pix_fmt yuv420p out.mp4
